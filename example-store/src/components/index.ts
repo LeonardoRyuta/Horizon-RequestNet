d24403fd54769f762a2d3bdb396c588447597198
Leo <leonardo.ryuta05@gmail.com>
2024-12-03 00:39:58 +0100

    example store done

--- example-store/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Navbar from "./Navbar";
-import ProductCard from "./ProductCard";
-
-export {
-  Navbar,
-  ProductCard
-}
--- example-store/src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-import Checkout from "./Checkout";
-import Storefront from "./StoreFront";
-
-export {
-  Checkout,
-  Storefront
-}
